ba74e0fc6c2862fb323fcc30bdd804f956aff348
albertraviss2023 <albertraviss@yahoo.com>
2025-06-14 00:06:12 +0300

    Update DAGs and jobs; add spark_test DAG and .gitignore

--- dags/spark_job_dag.py
+++ dags/spark_job_dag.py
@@ -1,17 +1,27 @@
-from datetime import datetime
 from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from datetime import datetime
 
+# Define the DAG
 with DAG(
     dag_id='spark_job_dag',
-    start_date=datetime(2025, 6, 10),
-    schedule_interval=None,
+    start_date=datetime(2025, 6, 12),
+    schedule_interval=None,  # Manual trigger
+    catchup=False,
 ) as dag:
-    spark_job = SparkSubmitOperator(
+
+    # Define the SparkSubmitOperator task
+    spark_task = SparkSubmitOperator(
         task_id='run_spark_job',
         application='/opt/spark_jobs/job.py',
         conn_id='spark_default',
-        executor_memory='1g',
-        total_executor_cores=1,
+        conf={
+            'spark.master': 'spark://172.18.0.4:7077',  # Correct master URL
+            'spark.total.executor.cores': '1',
+            'spark.executor.memory': '1g'
+        },
+        name='arrow-spark',
+        verbose=True,  # Enable verbose logging
     )
-    
+
+    spark_task  # Single task, no dependencies
