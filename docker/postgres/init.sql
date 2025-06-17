356d2e80cc1e74390273b6a4ff7db0cfbb237975
albertraviss2023 <albertraviss@yahoo.com>
2025-06-18 01:29:46 +0300

    update lakehouse code

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -1,22 +1,3 @@
--- Create user if it doesn't exist
-DO $$ 
-BEGIN
-    IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'airflow') THEN
-        CREATE USER airflow WITH PASSWORD 'airflow';
-    END IF;
-END $$;
-
--- Create database if it doesn't exist
-DO $$ 
-BEGIN
-    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'prod_db') THEN
-        CREATE DATABASE prod_db;
-    END IF;
-END $$;
-
--- Grant privileges to airflow user
+CREATE DATABASE prod_db;
+CREATE USER airflow WITH PASSWORD 'airflow';
 GRANT ALL PRIVILEGES ON DATABASE prod_db TO airflow;
-
--- Ensure airflow owns the public schema in prod_db
-ALTER SCHEMA public OWNER TO airflow;
-GRANT ALL ON SCHEMA public TO airflow;
